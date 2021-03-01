3ac443df333ac78fef7b44778b0ccf339565c32a
Amanda-G10 <reahana233@gmail.com>
2021-03-01 20:19:45 +0600

    add js

---
+++ MyFacebook/loginPg.js
@@ -0,0 +1,13 @@
+function check(form)
+{
+
+ if(form.userid.value =="Amanda" && form.pswrd.value =="123456")
+  {
+    window.open('http://arg-world.epizy.com')
+  }
+ else
+ {
+   alert("Error Password or Username")
+  }
+
+}
